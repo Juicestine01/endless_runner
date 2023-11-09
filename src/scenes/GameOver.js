1485bc2c8cfc6449ba6a4ca5bd5682410292a756
Justin Xu <jxu240@ucsc.edu>
2023-11-08 22:01:44 -0800

    added return to menu feature

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -7,12 +7,16 @@ class GameOver extends Phaser.Scene {
         this.add.text(game.config.width/2, game.config.height/3, 'GAME OVER', { fontFamily: 'After Hours', fontSize: 64 } ).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 +64, 'HighScore: ' + highScore, { fontFamily: 'After Hours', fontSize: 32 }).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 + 128, 'Press UP ARROW to Restart', { fontFamily: 'After Hours', fontSize: 32 } ).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 190, 'Press DOWN ARROW to go to Menu', { fontFamily: 'After Hours', fontSize: 32 } ).setOrigin(0.5);
         cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(cursors.up)) {
             this.scene.start('playScene')
+        }
+        if (Phaser.Input.Keyboard.JustDown(cursors.down)) {
+            this.scene.start('titleScene')
         }
     }
         
